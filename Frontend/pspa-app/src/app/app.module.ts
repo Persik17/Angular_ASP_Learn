aaf885a436b210154adc929f95661e00547be98e
Alexandr <ggwp@mail.ru>
2023-02-19 20:46:04 +0300

    added basic front navbar and template

---
+++ Frontend/pspa-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
+
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
